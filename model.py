46a8830d6b2765bba329974047b7a02327bb9c84
Patrick <synapse.and.syntax@gmail.com>
2012-10-01 21:07:38 -0400

    added gene class,minor MDL metaprogramming code

--- model.py
+++ model.py
@@ -1,9 +1,73 @@
+class Gene(object):
+    @staticmethod
+    def equals(a,b):
+        return "%s=%s\n" % (a,b)
 
+    def __init__(self,name,translation_rate,mRNA_degradation_rate,
+                 protein_degradation_rate,basal_transcription_rate,
+                 upstream_interactions):
+        self.name = name
+        self.translation_rate = translation_rate
+        self.mRNA_degradation_rate = mRNA_degradation_rate
+        self.protein_degradation_rate = protein_degradation_rate
+        self.basal_transcription_rate = basal_transcription_rate
+        self.upstream_interactions = upstream_interactions
+        self.promoter = self.name + "_promoter"
+        self.bound_promoter = self.name + "_bound_promoter"
+        self.mRNA = self.name + "_mRNA"
+        self.protein = self.name + "_protein"
+        self.init_promoter = 1
+        self.init_bound_promoter = 0
+        self.init_mRNA = 0
+        self.init_protein = 0
+
+        
+    def write_reaction(self,label,reactants,products,rate_factors):
+        #TODO: infer reaction rate from reactants
+        reaction_label = self.name + "_" + label + ":"
+        reactant_string = "+".join(map(str,reactants)) if reactants else "$pool"
+        product_string = "+".join(map(str,products)) if products else "$pool"
+        reaction_eq = "\t" + reactant_string + " > " + product_string
+        reaction_rate = "\t" + "*".join(map(str,rate_factors))
+        return "\n".join([reaction_label,
+                         reaction_eq,
+                         reaction_rate])    
+
+    def init_params(self):
+        return "\n".join([Gene.equals(self.promoter,self.init_promoter),
+                          Gene.equals(self.bound_promoter,self.init_bound_promoter),
+                          Gene.equals(self.mRNA,self.init_mRNA),
+                          Gene.equals(self.protein,self.init_protein)])
+        
+    def write(self):
+        """return a string describing all of gene's interactions'"""
+        param_string = self.init_params()
+        translation_string = self.write_reaction("translation",
+                                                 [self.mRNA],
+                                                 [self.mRNA,self.protein],
+                                                 [self.translation_rate,self.mRNA])
+        mRNA_deg_string = self.write_reaction("mRNA_degradation",
+                                              [self.mRNA],
+                                              [],
+                                              [self.mRNA_degradation_rate,self.mRNA])
+        protein_deg_string = self.write_reaction("protein_degradation",
+                                              [self.protein],
+                                              [],
+                                              [self.protein_degradation_rate,
+                                               self.protein])
+        basal_transcription_string = self.write_reaction("basal_transcription",
+                                              [self.promoter],
+                                              [self.promoter,self.mRNA],
+                                              [self.basal_transcription_rate,
+                                               self.promoter])
+        return "\n".join([param_string,translation_string,mRNA_deg_string,
+                         protein_deg_string, basal_transcription_string])
+        
 class Model(object):
     def __init__(self,genes,interactions):
         self.genes = genes
         self.interactions = interactions
-
+        
     def write(self,filename):
         with open(filename,'w') as f:
             f.write("# Reactions\n")
@@ -67,3 +131,5 @@ def main():
     mod = stochpy.SSA(File="foo.psc",dir='.')
     mod.DoStochSim(end=100000)
     mod.PlotTimeSim(species2plot=['X_protein','Y_protein','Z_protein'])
+
+print "loaded"
