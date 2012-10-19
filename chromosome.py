c74b3a5f59f5335be12c6169850da9c671d0b67e
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-10-19 13:17:38 -0400

    first commit on visualization function

--- chromosome.py
+++ chromosome.py
@@ -1,4 +1,5 @@
-import stochpy,random
+import stochpy,random,re
+import networkx as nx
 class Chromosome(object):
     serial_no = 0
     mrna_cost = 1
@@ -46,5 +47,35 @@ class Chromosome(object):
             genotype[random.randrange(Chromosome.num_params)] = r
         return Chromosome(genotype)
         
-    
+    def visualize(self):
+        g = nx.DiGraph()
+        nodes = ["U","V","X","Y","Z","T","P"]
+        node_label_templates = ["%s",
+                                 "%s_activated_transcription",
+                                 "%s_basal_transcription",
+                                 "%s_repressed_transcription"]
+        node_labels = [nlt % n for nlt in node_label_templates
+                       for n in nodes]
+        for node_label in node_labels:
+            if "transcription" in node_label:
+                rate = self.parse_model_file(node_label + ":")
+            else:
+                rate = 'blue'
+            g.add_node(node_label,color=rate)
+
+        pos = nx.spring_layout(g)
+        for i,(node_label,data) in enumerate(g.nodes(data=True)):
+            nx.draw_networkx_nodes(g,pos,nodelist = [node_label],
+                                   node_color=data['color'])
+        nx.draw_networkx_labels(g,pos,{label:label
+                                       for (label,data) in g.nodes(data=True)})
+
+    def parse_model_file(self,reaction_label):
+        with open("model%s.psc" % self.serial_no) as f:
+            lines = [line.strip() for line in f.readlines()]
+        reaction_line_index = lines.index(reaction_label)
+        rate_line = lines[reaction_line_index + 2]
+        rate = re.search(r"(\d.\d*)",rate_line).group()
+        return rate
+        
 print "loaded chromosome"
