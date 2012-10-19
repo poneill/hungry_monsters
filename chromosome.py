19ab99d18d1c666f56cee07787f7f809b9bf0928
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-10-19 01:59:24 -0400

    recombination and mutation operations

--- chromosome.py
+++ chromosome.py
@@ -7,11 +7,10 @@ class Chromosome(object):
     num_params = 94
     
     def __init__(self,genotype=None):
-        if genotype:
-            pre_genotype = genotype
+        if genotype is None:
+            self.genotype = [random.random() for i in range(Chromosome.num_params)]
         else:
-            pre_genotype = [random.random() for i in range(Chromosome.num_params)]
-        self.genotype = tuple(pre_genotype)
+            self.genotype = genotype
         self.cached_fitness = None
         self.serial_no = Chromosome.serial_no
         Chromosome.serial_no += 1
@@ -22,7 +21,10 @@ class Chromosome(object):
             return self.cached_fitness
         with open("template.psc") as f:
             template = "".join(f.readlines())
-            model = (template % self.genotype).replace("alpha",str(Chromosome.mrna_cost)).replace("beta",str(Chromosome.protein_cost)).replace("gamma",str(Chromosome.sugar_benefit))
+            model = (template % tuple(self.genotype))\
+                .replace("alpha",str(Chromosome.mrna_cost))\
+                .replace("beta",str(Chromosome.protein_cost))\
+                .replace("gamma",str(Chromosome.sugar_benefit))
             filename = "model%s.psc" % self.serial_no
             with open(filename,'w') as f:
                 f.write(model)
@@ -32,7 +34,17 @@ class Chromosome(object):
         self.cached_fitness = mod.data_stochsim.species[-1][atp_label]
         return self.cached_fitness
         
-    def foo(self):
-        print "bar!"
+    def recombine(self,other):
+        crossover_point = random.randrange(Chromosome.num_params)
+        return Chromosome(self.genotype[:crossover_point] +
+                          other.genotype[crossover_point:])
+
+    def mutate(self,points=1):
+        genotype = self.genotype
+        for point in range(points):
+            r = random.random()
+            genotype[random.randrange(Chromosome.num_params)] = r
+        return Chromosome(genotype)
         
+    
 print "loaded chromosome"
