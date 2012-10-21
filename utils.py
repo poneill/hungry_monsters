42936e13666f515681624fdabd3accf395a1e87f
Patrick <synapse.and.syntax@gmail.com>
2012-10-20 20:31:42 -0400

    added median function

--- utils.py
+++ utils.py
@@ -2,6 +2,13 @@
 def mean(xs):
     return sum(xs)/float(len(xs))
 
+def median(xs):
+    mp = int(len(xs)/2)
+    if len(xs) % 2 == 1:
+        return sorted(xs)[mp]
+    else:
+        return mean(sorted(xs)[mp-1:mp+1])
+    
 def variance(xs):
     return mean([x**2 for x in xs]) - mean(xs) ** 2
 
@@ -10,3 +17,5 @@ def zipWith(f,xs,ys):
 
 def transpose(xxs):
     return zip(*xxs)
+
+print "loaded utils"
