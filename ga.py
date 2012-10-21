e96f610a8c4427bf956e3373628d59d99571a663
Patrick <synapse.and.syntax@gmail.com>
2012-10-20 21:54:06 -0400

    various minor changes

--- ga.py
+++ ga.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot as plt
 
 class GA(object):
     def __init__(self,pop_size=100,k=2,init_pop = None):
-        self.pop_size = pop_size
         self.k = k
         self.recombination_rate = 0.89
         self.mutation_rate = 0.1
@@ -13,28 +12,38 @@ class GA(object):
         self.history = [] 
         if init_pop is None:
             self.population = [Chromosome() for i in range(pop_size)]
+            self.pop_size = pop_size
         else:
             self.population = init_pop
+            self.pop_size = len(init_pop)
+        self.gen = 0
+        self.record_history()
 
     def select_from_population(self,n):
         return [max([random.choice(self.population) for i in range(self.k)],
                    key=lambda chromosome: chromosome.fitness())
                 for j in range(n)]
-    def iterate(self):
-        self.record_history()
-        rates = [self.recombination_rate,self.mutation_rate,self.copy_rate]
-        params = map(lambda rate: int(rate*self.pop_size),rates)
-        if sum(params) < self.pop_size:
-            params[0] +=1
-        num_mutants,num_recombinants,num_copies = params
-        xs = self.select_from_population(num_recombinants)
-        ys = self.select_from_population(num_recombinants)
-        recombinants = [x.recombine(y) for (x,y) in zip(xs, ys)]
-        mutants = [m.mutate() for m in self.select_from_population(num_mutants)]
-        copies = self.select_from_population(num_copies)
+    
+    def iterate(self,iterations=1):
+        for iteration in range(iterations):
+            rates = [self.recombination_rate,self.mutation_rate,self.copy_rate]
+            params = map(lambda rate: int(rate*self.pop_size),rates)
+            if sum(params) < self.pop_size:
+                params[0] +=1
+                num_mutants,num_recombinants,num_copies = params
+                xs = self.select_from_population(num_recombinants)
+                ys = self.select_from_population(num_recombinants)
+                recombinants = [x.recombine(y) for (x,y) in zip(xs, ys)]
+                mutants = [m.mutate()
+                           for m in self.select_from_population(num_mutants)]
+                copies = self.select_from_population(num_copies)
+                self.population = recombinants + mutants + copies
+                self.record_history()
+                print[self.generation(),self.best_fitness(),self.mean_fitness()]
 
-        self.population = recombinants + mutants + copies
-
+    def generation(self):
+        return len(self.history)
+    
     def fitnesses(self):
         return [c.fitness() for c in self.population]
     
@@ -48,17 +57,26 @@ class GA(object):
         return max(self.fitnesses())
 
     def record_history(self):
-        self.history.append([self.best_fitness(),
-                             self.mean_fitness(),
-                             self.variance_fitness()])
+        self.history.append(self.fitnesses())
 
+    def best_chromosome(self):
+        return max([c for c in self.population],key=lambda x:x.fitness())
+    
     def plot(self):
-        bests,means = transpose(ga.history)
+        bests,means,medians = map(lambda f:map(f,(self.history)),
+                                    [max,mean,median])
         plt.plot(bests,label="best")
         plt.plot(means,label="mean")
+        plt.plot(medians,label="median")
+        reproduction_cutoff = 1/float(self.k) * self.pop_size
+        for gen_num,generation in enumerate(self.history):
+            for i,organism in enumerate(sorted(generation,reverse=True)):
+                org_color = "green" if i < reproduction_cutoff else "red"
+                plt.plot(gen_num,organism,color=org_color ,
+                         marker='o',linestyle="")
         plt.xlabel("generation")
         plt.ylabel("fitness (net ATP)")
-        plt.legend(loc=0)
+        plt.legend(loc=0,prop={'size':6})
         
         
 print "loaded ga"
